6e7566e08877ede694eb6a46fe4a65eb365f7183
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-05-03 14:52:01 +0000

    Add feature to modify icon color

--- multisearchviewlib/src/main/java/com/iammert/library/ui/multisearchviewlib/MultiSearchView.kt
+++ multisearchviewlib/src/main/java/com/iammert/library/ui/multisearchviewlib/MultiSearchView.kt
@@ -1,8 +1,11 @@
 package com.iammert.library.ui.multisearchviewlib
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
+import androidx.appcompat.content.res.AppCompatResources
 import com.iammert.library.ui.multisearchviewlib.databinding.ViewMultiSearchBinding
 import com.iammert.library.ui.multisearchviewlib.extensions.inflate
 
@@ -26,12 +29,17 @@ class MultiSearchView @JvmOverloads constructor(context: Context, attrs: Attribu
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.MultiSearchView, defStyleAttr, defStyleAttr)
         val searchTextStyle = typedArray.getResourceId(R.styleable.MultiSearchView_searchTextStyle, 0)
         val imageSource = typedArray.getResourceId(R.styleable.MultiSearchView_searchIcon, R.drawable.ic_round_search_24px)
-
+        val searchIconColor = typedArray.getResourceId(R.styleable.MultiSearchView_searchIconColor, android.R.color.black)
         binding.searchViewContainer.apply {
             this.searchTextStyle = searchTextStyle
         }
 
         setSearchIconDrawable(imageSource)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            setSearchIconColor(searchIconColor)
+        }
+
         binding.imageViewSearch.setOnClickListener {
             if (binding.searchViewContainer.isInSearchMode().not()) {
                 binding.searchViewContainer.search()
@@ -49,4 +57,10 @@ class MultiSearchView @JvmOverloads constructor(context: Context, attrs: Attribu
     fun setSearchIconDrawable(drawable: Int) {
         binding.imageViewSearch.setImageResource(drawable)
     }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun setSearchIconColor(color : Int) {
+        binding.imageViewSearch.imageTintList = AppCompatResources.getColorStateList(context, color)
+    }
+
 }
